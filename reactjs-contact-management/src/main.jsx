59a1c2b4d5202b51591201858fbba1cb36f167ae
ArilPopon <arilponconugroho@gmail.com>
2025-07-04 20:51:32 +0700

    feat : Register & Login

---
+++ reactjs-contact-management/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './components/layout'
+import UserRegister from './components/User/UserRegister'
+import UserLogin from './components/User/UserLogin'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/register' element={<UserRegister />} />
+          <Route path='/login' element={<UserLogin />} />
+        </Route>
+        <Route path='/dashboard'>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+)
