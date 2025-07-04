8d8708ff1851f04fef1c98572f1da199eadef695
ArilPopon <arilponconugroho@gmail.com>
2025-07-04 21:48:47 +0700

    Feat : dashboard, update user, logout

--- reactjs-contact-management/src/main.jsx
+++ reactjs-contact-management/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Layout from './components/layout'
 import UserRegister from './components/User/UserRegister'
 import UserLogin from './components/User/UserLogin'
+import DashboardLayout from './DashboardLayout'
+import UserProfile from './components/User/UserProfile'
+import UserLogout from './components/User/UserLogout'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,8 +16,10 @@ createRoot(document.getElementById('root')).render(
           <Route path='/register' element={<UserRegister />} />
           <Route path='/login' element={<UserLogin />} />
         </Route>
-        <Route path='/dashboard'>
-
+        <Route path='/dashboard' element={<DashboardLayout />}>
+          <Route path='contacts' element={<div>Contact</div>} />
+          <Route path='users/profile' element={<UserProfile />} />
+          <Route path='users/logout' element={<UserLogout />} />
         </Route>
       </Routes>
     </BrowserRouter>
